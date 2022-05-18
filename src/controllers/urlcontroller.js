3f386fa2b6361770369f23ebe5ad6d5780e627a5
Ranjitrajak <mail4ranjitrajak@gmail.com>
2022-05-18 21:48:37 +0530

    redis

--- src/controllers/urlcontroller.js
+++ src/controllers/urlcontroller.js
@@ -1,7 +1,29 @@
 const urlModel=require("../models/Urlmodel")
 const validUrl = require('valid-url')
 const shortid = require('shortid')
-const baseUrl = 'http:localhost:3000'
+const redis = require("redis");
+
+const { promisify } = require("util");
+
+//Connect to redis
+const redisClient = redis.createClient(
+  13782,
+  "redis-13782.c264.ap-south-1-1.ec2.cloud.redislabs.com",
+  { no_ready_check: true }
+);
+redisClient.auth("I8LuoTIJ38lm0Oj9DqoRyNkGRd6YopnW", function (err) {
+  if (err) throw err;
+});
+
+redisClient.on("connect", async function () {
+  console.log("Connected to Redis..");
+});
+//Connection setup for redis
+
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
+
+//start create short url---
 
 const createShorturl= async function(req,res){
 
@@ -9,31 +31,53 @@ const createShorturl= async function(req,res){
     if (!validUrl.isUri(longUrl)) {
         return res.status(400).json('Invalid base URL')
     }
-    let check=await urlModel.findOne({longUrl:longUrl})
-    if(check){
-        return res.status(409).send({status:false,message:"already exist"})
+    let alreadyCreate = await GET_ASYNC(`${longUrl}`)
+    let jsonData=JSON.parse(alreadyCreate)
+   //let check=await urlModel.findOne({longUrl:longUrl})
+    if(alreadyCreate){
+        return res.status(201).send({status:true,message:"succesfull",data:jsonData})
     }
-    const urlCode = shortid.generate(longUrl)
-    let url=await urlModel.findOne({urlCode:urlCode})
-    if(url){
-        return res.status(409).send({status:false,message:"already exist"})
-    }
+    
+    //let url=await urlModel.findOne({urlCode:urlCode})
+    //if(url){
+       //return res.status(409).send({status:false,message:"already exist"})
+       
+   // }
+   else{
+    const baseUrl = 'http:localhost:3000'
+    const urlCode = shortid.generate().toLowerCase()
     const shortUrl = baseUrl + '/' + urlCode
     const newUrl={longUrl,shortUrl,urlCode}
     const short=await urlModel.create(newUrl)
+
+    await SET_ASYNC(`${req.body.longUrl}`, JSON.stringify(short))
+    
+
     return res.status(201).send({status:true,data:short})
+}
 
 }
 const getlongurl=async function(req,res){
+  
     let urlCode=req.params.urlCode
-    let url=await urlModel.findOne({urlCode:urlCode})
-   // console.log(url)
-    if(url){
-        return res.status(302).redirect(url.longUrl)
+
+    if(!urlCode) return res.status(400).send({status : false, message : "Invalid request parameter. Please provide urlCode"})
+
+    let Url = await GET_ASYNC(`${urlCode}`)
+  
+   //console.log(Url)
+    if(Url){
+       return res.status(302).redirect(Url) 
     }
+   
+        
     else{
-    return res.status(404).send({status:false,message:"url not found"})
-}
+        let url= await urlModel.findOne({urlCode:urlCode})
+        await SET_ASYNC(`${req.params.urlCode}`, JSON.stringify(url.longUrl))
+        return res.status(302).redirect(url.longUrl) 
+    }
+    
+
 }
 
 module.exports={createShorturl,getlongurl}
